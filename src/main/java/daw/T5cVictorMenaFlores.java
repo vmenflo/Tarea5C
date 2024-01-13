edbf685084cd1ab1fb6d4718254bb65c0392786b
Victor <menaflores.victor@gmail.com>
2024-01-13 12:12:01 +0100

    Fase inicial

---
+++ src/main/java/daw/T5cVictorMenaFlores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author Víctor
+ */
+public class T5cVictorMenaFlores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
