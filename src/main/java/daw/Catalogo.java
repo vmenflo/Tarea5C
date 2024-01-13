98ea1c270480c89415b0d3e30cfaddcc56f9ea86
Victor <menaflores.victor@gmail.com>
2024-01-13 12:26:47 +0100

    Creacion objeto Catalogo basico sin metodos

--- src/main/java/daw/Catalogo.java
+++ src/main/java/daw/Catalogo.java
@@ -4,10 +4,63 @@
  */
 package daw;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  *
  * @author Víctor
  */
 public class Catalogo {
+    //Atributos
+    private ArrayList<Cerveza> lista = new ArrayList<>();
+    
+    //Contructores
+    //Sin elementos
+    public Catalogo() {
+    }
+    
+    // Getter and Setter
+    public ArrayList<Cerveza> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Cerveza> lista) {
+        this.lista = lista;
+    }
+    
+    //To String
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Catalogo{");
+        sb.append("lista=").append(lista);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    //Equals y HashCode
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 19 * hash + Objects.hashCode(this.lista);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Catalogo other = (Catalogo) obj;
+        return Objects.equals(this.lista, other.lista);
+    }
+    
     
 }
