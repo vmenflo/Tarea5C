5be8e9727e6add40a05866c597204b93ad7c7f19
Victor <menaflores.victor@gmail.com>
2024-01-13 15:33:39 +0100

    Metodos de catalogos terminado

--- src/main/java/daw/Catalogo.java
+++ src/main/java/daw/Catalogo.java
@@ -5,6 +5,8 @@
 package daw;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Objects;
 
 /**
@@ -12,14 +14,99 @@ import java.util.Objects;
  * @author Víctor
  */
 public class Catalogo {
+
     //Atributos
     private ArrayList<Cerveza> lista = new ArrayList<>();
-    
-    //Contructores
-    //Sin elementos
+    private Comparator<Cerveza> ordenarNombre
+            = (e1, e2)
+            -> e1.getNombre().compareToIgnoreCase(e2.getNombre());
+     private Comparator<Cerveza> ordenarGraduacion
+            = (e1, e2)
+            -> Integer.compare(e1.getGraduacion(), e2.getGraduacion());
+    //METODOS
+
+    //Contructor Sin elementos
     public Catalogo() {
     }
+
+    //Devuelve el número de elementos
+    public int numeroElementos() {
+        return lista.size();
+    }
+
+    //Devuelve si está vacio
+    public boolean estaVacio() {
+        return lista.isEmpty();
+    }
+
+    //Devuelve un elemento
+    public void verElemento(int posicion) {
+        System.out.println(lista.get(posicion));
+    }
+
+    //Cambia elemento
+    public void cambiarElemento(int posicion, Cerveza cerveza) {
+        lista.add(posicion, cerveza);
+    }
+
+    //Guardar elemento
+    public void guardarElemento(Cerveza cerveza) {
+        lista.add(cerveza);
+    }
+
+    //Eleminar elemento
+    public void eliminarElemento(int posicion) {
+        lista.remove(posicion);
+    }
+
+    //Eliminar elemento especificado
+    public void eliminarElemento(Cerveza cerveza) {
+        lista.remove(cerveza);
+    }
+
+    //eliminarElementos
+    public void eliminarElementos(ArrayList<Cerveza> listaABorrar) {
+        lista.removeAll(listaABorrar);
+    }
+
+    //Eliminar todos los elementos
+    public void eliminarTodos() {
+        lista.removeAll(lista);
+    }
+
+    //Imprimir el catalogo
+    public void imprimirCatalogo() {
+        lista.forEach(System.out::println);
+    }
+
+    //Buscar un objeto en la lista y devuelve posicion
+    public int buscarElemento(Cerveza c) {
+        return lista.indexOf(c);
+    }
+
+    //Metodos de ordenacion usando sort
+    //Por nombre
+    public void ordenarNombre() {
+        lista.sort(ordenarNombre);
+    }
+    //Por graduacion
+    public void ordenarGraduacion(){
+        lista.sort(ordenarGraduacion);
+    }
     
+    //Por graduación y nombre
+    public void ordenarGraduacionNombre(){
+        lista.sort(ordenarGraduacion.thenComparing(ordenarNombre));
+    }
+    
+    //Busqueda
+    public int buscarElementoBinario(Cerveza c){
+       Cerveza x = new Cerveza();
+       x.setNombre(c.getNombre());
+       return Collections.binarySearch(lista, x, ordenarNombre);
+        
+    }
+
     // Getter and Setter
     public ArrayList<Cerveza> getLista() {
         return lista;
@@ -28,7 +115,7 @@ public class Catalogo {
     public void setLista(ArrayList<Cerveza> lista) {
         this.lista = lista;
     }
-    
+
     //To String
     @Override
     public String toString() {
@@ -38,7 +125,7 @@ public class Catalogo {
         sb.append('}');
         return sb.toString();
     }
-    
+
     //Equals y HashCode
     @Override
     public int hashCode() {
@@ -61,6 +148,5 @@ public class Catalogo {
         final Catalogo other = (Catalogo) obj;
         return Objects.equals(this.lista, other.lista);
     }
-    
-    
+
 }
